19d6c90956d6b8caa5a9fbd09e399991b72ea4d2
maszynka <respown@gmail.com>
2016-10-22 22:39:11 +0200

    JS a bit better HTML injection

--- js/content_script.js
+++ js/content_script.js
@@ -2,5 +2,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 
   chrome.tabs.executeScript(null, {file: "js/wyah.js"});
-  chrome.tabs.insertCSS(null, {file: "css/wyah.min.css"});
+  chrome.tabs.insertCSS(null, {file: "css/wyah.css"});
 });
+
--- js/wyah.js
+++ js/wyah.js
@@ -1,10 +1,10 @@
 console.log('Wyah initiated!');
 var wrapper = document.createElement("div"); // Create Div wrapper
 var span = document.createElement("span");
-var spanText = document.createTextNode("Why you are here?"); // Create a text node
+var spanText = document.createTextNode("Why you are visiting this website?"); // Create a text node
 var input = document.createElement("Input");
 span.appendChild(spanText);
-input.placeholder="what do you want to get from the website";
+input.placeholder="{Your reason}";
 
 wrapper.appendChild(span);
 wrapper.appendChild(input);
@@ -12,3 +12,26 @@ wrapper.id="wyah__wrap"; // Append the text to <li>
 input.type="Text";
 
 document.body.appendChild(wrapper); // Append <li> to <ul> with id="myList"
+
+//Auto growing submit
+var textContainer, textareaSize, input;
+var autoSize = function () {
+    textareaSize.innerHTML = input.value + '\n';
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    textContainer = document.querySelector('.textarea-container');
+    textareaSize = textContainer.querySelector('.textarea-size');
+    input = textContainer.querySelector('textarea');
+
+    autoSize();
+    input.addEventListener('input', autoSize);
+});
+// Autogrowing submit END
+
+//Colapse on submit
+
+var ele = document.getElementById("xyz"); // faster than query selector
+if(ele.addEventListener) {
+    ele.addEventListener("submit", callback, false);
+}
