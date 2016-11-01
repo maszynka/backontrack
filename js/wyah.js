7db7aa8a3ab596daf4219258f6f8f6933665233a
maszynka <respown@gmail.com>
2016-11-01 23:35:23 +0100

    Working on Shadow DOM now, added dissotiation effect to a page content

--- js/wyah.js
+++ js/wyah.js
@@ -4,26 +4,49 @@ console.log('Wyah JS has started!');
 
 //Checks:
 
-var wyahExists = document.getElementById("wyah-wrap"); //Check if extensions code exists in a DOM
+var wyahExists = document.getElementById("wyah"); //Check if extensions code exists in a DOM
 //var supportShadowDOM = document.head.createShadowRoot; //Check ShadowDOM support
 
 //Wyah Init - extension's HTML injection into a DOM
 function wyahInit() {
     console.log("wyah: initiated");
     // HTML string
+    var shadomDomTest = '<span>ShadowDOM is not supported in your browser! <a href="http://caniuse.com/#feat=shadowdom">Check Can I use</a></span>';
     var htmlString = '<form id="wyah-form">'
                     + ' <input type="text" placeholder="Why are you here?"/>'
                     + ' <label><input id="wyah-f-minutes" type="number" placeholder="5"/>minutes</label>'
                     + '</form>';
         //+ '<span contenteditable="true">sdfsd</span>'
-        ;
+    // The Shadow DOM part
 
     var div = document.createElement('div');
-    div.id = "wyah-wrap";
-    div.innerHTML = htmlString;
-    document.body.appendChild(div); // Append <li> to <ul> with id="myList"
+    div.id = "wyah";
+    // Trying to inject shadow dom
+    var root = div.createShadowRoot();
+    root.innerHTML = htmlString;
+    // end of Shadow dom part
+    //If ShadowDOm in not availavble insert in standard way with communicate
+    div.innerHTML = shadomDomTest + htmlString;
+
+    //document.body.insertBefore(div, document.body.firstChild);
+    document.documentElement.appendChild(div, document.body.firstChild);
+    //document.body.appendChild(div); // Append <li> to <ul> with id="myList"
 }
 
+function dissociationSwitch() {
+    //if (switch==false)
+    if (document.documentElement.classList.contains('dissociated')) {
+        document.documentElement.classList.remove('dissociated');
+        console.log('Class .dissociated should be removed');
+    }
+    else {
+
+        document.documentElement.classList.add('dissociated');
+        console.log('Class .dissociated should be added');
+
+    }
+}
+/*
 function currentInput() {
     console.log('currentInput initiated')
     var focusedInput = document.getElementById("wyah-wrap").activeElement;
@@ -34,15 +57,17 @@ function currentInput() {
             $(this).next().focus();
         }
     });
-}
+}*/
 
 // Check: if wyah exists in a DOM
 
+dissociationSwitch();
 if (!wyahExists) {
     wyahInit();
-    currentInput();
+    //currentInput();
 }
 else {
+    console.log('Wayh already exists!');
     //Wyah was already initiated and DOM is injected
 }
 
