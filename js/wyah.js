b2339e5a52cd0f594720ecc0fe21fe9ac496e991
maszynka <respown@gmail.com>
2016-11-01 15:26:19 +0100

    Version before switching to shadow dom

--- js/wyah.js
+++ js/wyah.js
@@ -1,17 +1,52 @@
-console.log('Wyah initiated!');
-var wrapper = document.createElement("div"); // Create Div wrapper
-var span = document.createElement("span");
-var spanText = document.createTextNode("Why you are visiting this website?"); // Create a text node
-var input = document.createElement("Input");
-span.appendChild(spanText);
-input.placeholder="{Your reason}";
+//Extension's JS has started
 
-wrapper.appendChild(span);
-wrapper.appendChild(input);
-wrapper.id="wyah__wrap"; // Append the text to <li>
-input.type="Text";
+console.log('Wyah JS has started!');
 
-document.body.appendChild(wrapper); // Append <li> to <ul> with id="myList"
+//Checks:
+
+var wyahExists = document.getElementById("wyah-wrap"); //Check if extensions code exists in a DOM
+//var supportShadowDOM = document.head.createShadowRoot; //Check ShadowDOM support
+
+//Wyah Init - extension's HTML injection into a DOM
+function wyahInit() {
+    console.log("wyah: initiated");
+    // HTML string
+    var htmlString = '<form id="wyah-form">'
+                    + ' <input type="text" placeholder="Why are you here?"/>'
+                    + ' <label><input id="wyah-f-minutes" type="number" placeholder="5"/>minutes</label>'
+                    + '</form>';
+        //+ '<span contenteditable="true">sdfsd</span>'
+        ;
+
+    var div = document.createElement('div');
+    div.id = "wyah-wrap";
+    div.innerHTML = htmlString;
+    document.body.appendChild(div); // Append <li> to <ul> with id="myList"
+}
+
+function currentInput() {
+    console.log('currentInput initiated')
+    var focusedInput = document.getElementById("wyah-wrap").activeElement;
+    //document.getElementById("wyah-wrap").addEventListener('focusin', function(e) { console.log('focusin!' + e + "\n lets check \"activeElement: " + focusedInput)});
+    console.log("Currently selected: " + focusedInput);
+    $('input[type="textbox"]').keyup(function(e) {
+        if(e.keyCode == 13) {
+            $(this).next().focus();
+        }
+    });
+}
+
+// Check: if wyah exists in a DOM
+
+if (!wyahExists) {
+    wyahInit();
+    currentInput();
+}
+else {
+    //Wyah was already initiated and DOM is injected
+}
+
+
 
 //Auto growing submit
 var textContainer, textareaSize, input;
@@ -30,8 +65,13 @@ document.addEventListener('DOMContentLoaded', function() {
 // Autogrowing submit END
 
 //Colapse on submit
-
+/*
 var ele = document.getElementById("xyz"); // faster than query selector
 if(ele.addEventListener) {
     ele.addEventListener("submit", callback, false);
-}
+}*/
+
+/*
+ TODO: Add list of websites that the code will run at every time
+ TODO: Consider using shadowDOM
+ */
